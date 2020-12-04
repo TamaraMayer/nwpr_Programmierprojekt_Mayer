b78a9fa2ba146c54330061c6911718f840ab8486
Tamara Mayer <tamara-mayer@outlook.com>
2020-12-04 17:02:19 +0100

    added code to client to read messages from server, and write it to the console

--- zuHauseVersuch/zuHauseVersuch/Client.cs
+++ zuHauseVersuch/zuHauseVersuch/Client.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace Client
@@ -13,7 +16,29 @@ namespace Client
 
         internal void Start(int port)
         {
-            throw new NotImplementedException();
+            IPAddress ip_adress = IPAddress.Parse("127.0.0.1");
+            //    try
+            //    {
+
+            TcpClient tcpClient = new TcpClient(ip_adress.ToString(), port);
+            Console.WriteLine("Connection successful");
+            Console.WriteLine("Enter 'Exit' to teminate");
+
+            StreamReader reader = new StreamReader(tcpClient.GetStream());
+
+            while (true)
+            {
+                string serverString = reader.ReadLine();
+                Console.WriteLine(serverString);
+            }
+
+            reader.Close();
+            tcpClient.Close();
+            //}
+            //catch(Exception e)
+            //{
+
+            //}
         }
     }
 }

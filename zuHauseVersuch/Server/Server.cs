28e8b655dcbfb446c5ea0a2fe212ffa3b4bfaaf8
Tamara Mayer <tamara-mayer@outlook.com>
2020-12-04 16:16:58 +0100

    added code to server

--- zuHauseVersuch/Server/Server.cs
+++ zuHauseVersuch/Server/Server.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace Server
 {
     public class Server
     {
-        internal void Start(int port)
+        public void Start(int port)
+        {
+            try
+            {
+
+                TcpListener tcpListener = new TcpListener(IPAddress.Any, port);
+                tcpListener.Start();
+
+                Console.WriteLine("Server started");
+
+                while (true)
+                {
+                    TcpClient client = tcpListener.AcceptTcpClient();
+                    Thread t = new Thread(CommunicateWithClient);
+                    t.Start(client);
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+
+        }
+
+        private void CommunicateWithClient(object obj)
         {
             throw new NotImplementedException();
         }
